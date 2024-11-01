56a9c3d40df822afa925f58b4311c31c5e89d455
stellamarma <stellamarmat@gmail.com>
2024-11-01 10:08:39 +0200

    Add the delete method

--- 2/book-management-system/src/app/book/book.component.ts
+++ 2/book-management-system/src/app/book/book.component.ts
@@ -11,9 +11,35 @@ export class BookComponent {
   bookTitle: string = '';
   bookAuthor: string = '';
 
+  // 1. Προσθήκη βιβλίου στο localStorage
   addBook() {
-    console.log('Book added:', this.bookTitle, this.bookAuthor);
+    const newBook = { title: this.bookTitle, author: this.bookAuthor };
+    let books = this.retrieveBooks();
+
+    books.push(newBook);  // Προσθέτουμε το νέο βιβλίο στη λίστα
+
+    // Αποθήκευση της λίστας στο localStorage
+    localStorage.setItem('books', JSON.stringify(books));
+
+    // Καθαρίζουμε τα πεδία εισαγωγής
     this.bookTitle = '';
     this.bookAuthor = '';
+  }
+
+  // 2. Ανάκτηση όλων των βιβλίων από το localStorage
+  retrieveBooks() {
+    const books = localStorage.getItem('books');
+    return books ? JSON.parse(books) : [];  // Επιστροφή λίστας βιβλίων ή κενής λίστας
+  }
+
+  // 3. Διαγραφή βιβλίου από το localStorage
+  deleteBook(index: number) {
+    let books = this.retrieveBooks();
+
+    // Αφαιρούμε το βιβλίο από τον πίνακα
+    books.splice(index, 1);
+
+    // Ενημερώνουμε το localStorage
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
