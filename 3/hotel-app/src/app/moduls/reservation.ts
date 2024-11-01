f76259055703d30ecdaa5e29654425cbf401720b
stellamarma <stellamarmat@gmail.com>
2024-11-01 11:46:28 +0200

    Create interface reservation

---
+++ 3/hotel-app/src/app/moduls/reservation.ts
@@ -0,0 +1,8 @@
+export interface Reservation {
+    id:string,
+    checkInDate: Date,
+    checkOutDate: Date,
+    guessName: string,
+    guessEmail: string,
+    roomNumber: number
+}
--- 3/hotel-app/src/app/reservation/reservation.module.ts
+++ 3/hotel-app/src/app/reservation/reservation.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 
 
 
 @NgModule({
   declarations: [
-    ReservationFormComponent
+    ReservationFormComponent,
+    ReservationListComponent
   ],
   imports: [
     CommonModule
